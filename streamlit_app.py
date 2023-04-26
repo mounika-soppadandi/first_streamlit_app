a2148036605876f22c0c652a3bcec23fdf8b81d0
mounika-soppadandi <46266004+mounika-soppadandi@users.noreply.github.com>
2023-04-26 22:16:41 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -37,12 +37,26 @@ try:
 except:
   streamlit.error("Error occurred while retrieving data from Fruityvice API.")
 
-streamlit.header ("The fruit load list contains:")
+'''streamlit.header ("The fruit load list contains:")
 #Snowflake-related functions
 def get_fruit_load_list():
   with my_cnx.cursor() as my_cur:
     my_cur.execute("select * from fruit_load_list")
-    return my_cur.fetchall()
+    return my_cur.fetchall()'''
+# Allow the end user to add a fruit to the list
+def insert_row_snowflake(new_fruit):
+    with my_cnx.cursor() as my_cur:
+        my_cur.execute("insert into fruit_load_list values ('" + new_fruit + "')")
+    return "Thanks for adding " + new_fruit
+
+# Add new fruits to the list
+new_fruits = ["jackfruit", "papaya", "guava", "kiwi"]
+for fruit in new_fruits:
+    insert_row_snowflake(fruit)
+    
+# Remove test and from streamlit rows
+delete_rows_snowflake(["test", "from streamlit"])
+
 # Add a button to load the fruit
 if streamlit.button('Get Fruit Load List'):
   my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
