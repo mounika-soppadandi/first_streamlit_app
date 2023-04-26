f6710e4d7f0b9d8a0efc69fbaa8604a018574466
mounika-soppadandi <46266004+mounika-soppadandi@users.noreply.github.com>
2023-04-26 22:05:36 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -37,3 +37,14 @@ try:
 except:
   streamlit.error("Error occurred while retrieving data from Fruityvice API.")
 
+streamlit.header ("The fruit load list contains:")
+#Snowflake-related functions
+def get_fruit_load_list():
+  with my_cnx.cursor() as my_cur:
+    my_cur.execute("select * from fruit_load_list")
+    return my_cur.fetchall()
+# Add a button to load the fruit
+if streamlit.button('Get Fruit Load List'):
+  my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
+  my_data_rows = get_fruit_load_list()
+  streamlit.dataframe(my_data_rows)
