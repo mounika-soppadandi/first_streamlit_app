58c675aee6fcc90cc15933a29ac5ae352486073c
mounika-soppadandi <46266004+mounika-soppadandi@users.noreply.github.com>
2023-04-26 22:54:47 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -39,9 +39,9 @@ except:
 
 streamlit.header ("The fruit load list contains:")
 #Snowflake-related functions
-def get_fruit_load_list(new_fruit):
+def get_fruit_load_list():
   with my_cnx.cursor() as my_cur:
-    my_cur.execute("INSERT INTO fruit_load_list VALUES ('" + new_fruit + "')")
+    my_cur.execute("select * from fruit_load_list")
     return my_cur.fetchall()
   
 # Function to insert a row into the fruit_load_list table in Snowflake
